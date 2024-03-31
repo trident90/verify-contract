c9ccf3edb4aaf218b1fe0ac9ffe03b71711918a2
trident <trident@naver.com>
2024-03-31 14:45:13 +0000

    update

---
+++ src/main/java/io/cplabs/wemixfi/verify/GetSourceResponse.java
@@ -0,0 +1,162 @@
+package io.cplabs.wemixfi.verify;
+
+public class GetSourceResponse {
+    private String status;
+    private String message;
+    private String results;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResults() {
+        return results;
+    }
+
+    public void setResults(String results) {
+        this.results = results;
+    }
+
+    // public List<Result> getResults() {
+    //     return results;
+    // }
+
+    // public void setResults(List<Result> results) {
+    //     this.results = results;
+    // }
+
+    // public static class Result {
+
+    //     private String SourceCode;
+    //     private String ABI;
+    //     private String ContractName;
+    //     private String CompilerVersion;
+    //     private String OptimizationUsed;
+    //     private String Runs;
+    //     private String ConstructorArguments;
+    //     private String EVMVersion;
+    //     private String Library;
+    //     private String LicenseType;
+    //     private String Proxy;
+    //     private String Implementation;
+    //     private String SwarmSource;
+
+    //     // Getter and Setter methods here...
+    //     public String getSourceCode() {
+    //         return SourceCode;
+    //     }
+
+    //     public void setSourceCode(String sourceCode) {
+    //         this.SourceCode = sourceCode;
+    //     }
+        
+    //     public String getABI() {
+    //         return ABI;
+    //     }
+
+    //     public void setABI(String abi) {
+    //         this.ABI = abi;
+    //     }
+        
+    //     public String getContractName() {
+    //         return ContractName;
+    //     }
+
+    //     public void setContractName(String contractName) {
+    //         this.ContractName = contractName;
+    //     }
+        
+    //     public String getCompilerVersion() {
+    //         return CompilerVersion;
+    //     }
+
+    //     public void setCompilerVersion(String compilerVersion) {
+    //         this.CompilerVersion = compilerVersion;
+    //     }
+        
+    //     public String getOptimizationUsed() {
+    //         return OptimizationUsed;
+    //     }
+
+    //     public void setOptimizationUsed(String optimizationUsed) {
+    //         this.OptimizationUsed = optimizationUsed;
+    //     }
+        
+    //     public String getRuns() {
+    //         return Runs;
+    //     }
+
+    //     public void setRuns(String runs) {
+    //         this.Runs = runs;
+    //     }
+        
+    //     public String getConstructorArguments() {
+    //         return ConstructorArguments;
+    //     }
+
+    //     public void setConstructorArguments(String constructorArguments) {
+    //         this.ConstructorArguments = constructorArguments;
+    //     }
+        
+    //     public String getEVMVersion() {
+    //         return EVMVersion;
+    //     }
+
+    //     public void setEVMVersion(String evmVersion) {
+    //         this.EVMVersion = evmVersion;
+    //     }
+        
+    //     public String getLibrary() {
+    //         return Library;
+    //     }
+
+    //     public void setLibrary(String library) {
+    //         this.Library = library;
+    //     }
+        
+    //     public String getLicenseType() {
+    //         return LicenseType;
+    //     }
+
+    //     public void setLicenseType(String licenseType) {
+    //         this.LicenseType = licenseType;
+    //     }
+        
+    //     public String getProxy() {
+    //         return Proxy;
+    //     }
+
+    //     public void setProxy(String proxy) {
+    //         this.Proxy = proxy;
+    //     }
+        
+    //     public String getImplementation() {
+    //         return Implementation;
+    //     }
+
+    //     public void setImplementation(String implementation) {
+    //         this.Implementation = implementation;
+    //     }
+        
+    //     public String getSwarmSource() {
+    //         return SwarmSource;
+    //     }
+
+    //     public void setSwarmSource(String swarmSource) {
+    //         this.SwarmSource = swarmSource;
+    //     }
+        
+    // }
+}
--- src/main/java/io/cplabs/wemixfi/verify/VerifyController.java
+++ src/main/java/io/cplabs/wemixfi/verify/VerifyController.java
@@ -1,6 +1,7 @@
 package io.cplabs.wemixfi.verify;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,5 +37,43 @@ public class VerifyController {
         response.setValid(isValid);
 
         return ResponseEntity.ok().body(response);
-    };
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<GetSourceResponse> getSource(
+            @RequestParam String apikey,
+            @RequestParam String module,
+            @RequestParam String action,
+            @RequestParam String address) {
+        System.out.println("apikey =" + apikey);
+        System.out.println("module =" + module);
+        System.out.println("action =" + action);
+        System.out.println("address =" + address);
+
+        // Response
+        GetSourceResponse response = new GetSourceResponse();
+        response.setStatus("0");
+        response.setMessage("NOK");
+        response.setResults("Contract source code not verified");
+        // List<Result> results = new ArrayList<Result>();
+
+        // Result result = new Result();
+        // result.setSourceCode("");
+        // result.setABI("");
+        // result.setCompilerVersion("");
+        // result.setConstructorArguments("");
+        // result.setContractName("");
+        // result.setLicenseType("");
+        // result.setLibrary("");
+        // result.setOptimizationUsed("");
+        // result.setEVMVersion("");
+        // result.setProxy("");
+        // result.setRuns("");
+        // result.setImplementation("");
+        // result.setSwarmSource("");
+
+        // results.add(result);
+        // response.setResults(results);
+        return ResponseEntity.ok().body(response);
+    }
 }
